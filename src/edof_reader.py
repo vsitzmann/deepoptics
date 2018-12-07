6ff368f7113546d92be2904a3a77adab8d930eba
Vincent Sitzmann <sitzmann@cs.stanford.edu>
2018-12-06 18:59:10 -0800

    Comments

--- src/edof_reader.py
+++ src/edof_reader.py
@@ -8,10 +8,19 @@ from glob import glob
 def get_edof_training_data(target_dir,
                            patch_size,
                            batch_size,
-                           log_depth_sampling=True,
                            num_depths=4,
                            num_par_calls=4,
                            repeat=True):
+    '''Data reader that ingests images and cuts out patches with patch_size.
+
+    :param target_dir (str): Directory containing images.
+    :param patch_size (str): Size of the patches to be extracted from images.
+    :param batch_size: Batch size.
+    :param num_depths: Number of depth planes that a single image is placed at.
+    :param num_par_calls: Number of parallel readers.
+    :param repeat: Whether to loop the dataset.
+    :return:
+    '''
     def parse_img(img_path):
         img_string = tf.read_file(img_path)
         img_decoded = tf.image.decode_jpeg(img_string, channels=3)
@@ -19,7 +28,11 @@ def get_edof_training_data(target_dir,
         return img
 
     def augment_img(img):
-        return tf.random_crop(img, [patch_size, patch_size, 3])
+        return tf.image.resize_image_with_crop_or_pad(
+            img,
+            patch_size,
+            patch_size
+        )
 
     img_path_list = glob(os.path.join(target_dir, '*.jpg')) + glob(os.path.join(target_dir, '*.png'))
     img_dataset = Dataset.from_tensor_slices((img_path_list))
@@ -37,6 +50,7 @@ def get_edof_training_data(target_dir,
     img_dataset = img_dataset.prefetch(4*batch_size)
 
     image_batch = img_dataset.make_one_shot_iterator().get_next()
+    image_batch.set_shape((batch_size, patch_size, patch_size, 3))
 
     all_depths = tf.convert_to_tensor([1/2, 1/1.5, 1/1, 1/0.5, 1000], tf.float32)
     depth_bins = []
